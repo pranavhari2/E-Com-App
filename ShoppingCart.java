a0dce1b1ddc4600ac142e462c6930fc419b12a7c
Pranav Hari <pranavbts2001@gmail.com>
2021-03-01 12:22:59 -0500

    Updated with new classes

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,2 +1,83 @@
+import java.util.ArrayList;
+
 public class ShoppingCart {
+
+    private ArrayList<Product> products = new ArrayList<>();   // ArrayList of Products
+    private ArrayList<Integer> quantity = new ArrayList<>();    // Array of Quantity
+
+    public void addProduct (Product product) {
+        products.add(product);
+    }
+
+
+    /**
+     * Retrives the product given the product ID.
+     *
+     * @param id Product id to be checked
+     * @return Product Product with the same ID.
+     */
+
+    public Product getProduct (int id) {
+        for (int i = 0; i <= products.size(); i++) {
+            if (products.get(i).getID() == id) {
+                return products.get(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves the stock for a product given a product ID
+     *
+     * @param id Product id to check for
+     * @return int Returns stock of specified item id
+     */
+
+    public int getStock (int id) {
+        for (int i = 0; i <= products.size(); i++) {
+            if (products.get(i).getID() == id) {
+                return quantity.get(i);
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Increases a products stock given the product and the amount of stock to be added.
+     *
+     * @param product Product to add
+     * @param stock  Amount of product to add
+     */
+
+    public void addStock (Product product, int stock) {
+        for (int i = 0; i <= products.size(); i++) {
+            if (products.get(i).equals(product)) {
+                this.quantity.set(i,this.quantity.get(i)+ stock);
+            }
+        }
+        products.add(product);
+        this.quantity.set(products.size()-1,this.quantity.get(products.size()-1) + stock);
+    }
+
+    /**
+     * Decreases a products stock given the product and the amount of stock to be added.
+     * Sets a product's stock to default if stock is below 0.
+     *
+     * @param product Product to add
+     * @param stock  Amount of product to remove
+     */
+
+    public void removeStock (Product product, int stock) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).equals(product)) {
+                if (this.quantity.get(i) - stock < 0){
+                    this.quantity.set(i,0);
+                }
+                else{
+                    this.quantity.set(i, this.quantity.get(i)-stock);
+                }
+            }
+        }
+    }
 }
+
--- StoreView.java
+++ StoreView.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class StoreView {
+
+    public static void main(String[] args) {
+        StoreManager sm = new StoreManager();
+        StoreView sv1 = new StoreView(sm, sm.assignNewCartID()); StoreView sv2 = new StoreView(sm, sm.assignNewCartID()); StoreView sv3 = new StoreView(sm, sm.assignNewCartID()); StoreView[] users = {sv1, sv2, sv3};
+        int activeSV = users.length;
+        Scanner sc = new Scanner(System.in);
+        while (activeSV > 0) {
+            System.out.print("CHOOSE YOUR STOREVIEW >>> ");
+            int choice = sc.nextInt();
+            if (choice < users.length && choice >= 0) {
+                if (users[choice] != null) {
+                    String chooseAnother = "";
+                    while (!chooseAnother.equals("y") && !chooseAnother.equals("Y")) {
+// this implementation of displayGUI waits for input and displays the page // corresponding to the user's input. it does this once, and then returns // true if the user entered 'checkout' or 'quit'.
+                        if (users[choice].displayGUI()) {
+                            users[choice] = null; activeSV--;
+                            break;
+                        }
+                        System.out.print("GO TO ANOTHER STOREVIEW? (y) >>> "); chooseAnother = sc.next();
+                    }
+                } else {
+                    System.out.println("MAIN > ERROR > BAD CHOICE\nTHAT STOREVIEW WAS DEACTIVATED"); }
+            } else { System.out.println(
+                    String.format("MAIN > ERROR > BAD CHOICE\nPLEASE CHOOSE IN RANGE [%d, %d]", 0, users.length - 1)
+            ); }
+        }
+        System.out.println("ALL STOREVIEWS DEACTIVATED"); }
 }
