9d2a9cc9f12d4bd6b5e824a41a7e58a1d2a33b6f
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2021-09-13 18:44:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Type \'hellllpppp\' for a list of commands.");
+        System.out.println("Type \'help\' for a list of commands.");
     }
 }
